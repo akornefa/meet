6790fb15b9185aac5eb11a8f3fa2ef9464575acf
Akornefa Akyea <64564297+akornefa@users.noreply.github.com>
2021-08-10 18:03:36 -0500

    edits

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -14,7 +14,7 @@ const credentials = {
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
   redirect_uris: ['https://akornefa.github.io/meet/'],
   javascript_origins: ['https://akornefa.github.io", "http://localhost:3000'],
-}; akornefa
+};
 
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
