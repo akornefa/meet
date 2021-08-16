91ff22a0c23c23695d900dcdcc3f77d8851b34de
Akornefa Akyea <64564297+akornefa@users.noreply.github.com>
2021-08-16 14:58:44 -0500

    Implementing "show Details" button

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,42 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+
+  state = {
+    showDetails: false
+  }
+
+  handleButton = (event) => {
+    this.setState({ showDetails: !showDetails });
+  };
   render() {
-    return <div></div>;
+
+    let event = this.props.event;
+
+    return <div>
+      <div className='basicInfo'>
+        <p>{event.start.dateTime}</p>
+        <h2>{event.summary}</h2>
+        <p>{event.location}</p>
+        {/* not sure how to implement number of people going or people hosting?? */}
+      </div>
+      {/* if (this.state.showDetails === true) return (<p>{event.description}</p>) */}
+      if (this.state.showDetails === false) return
+      (<button
+        type='submit'
+        className='detailsButton showDetails'
+        onClick={() => this.handleButton()}>
+        More
+      </button>);
+      if (this.state.showDetails === true) return
+      ((<p>{event.description}</p>)
+      <button
+        type='submit'
+        className='detailsButton showDetails'
+        onClick={() => this.handleButton()}>
+        Details
+      </button>);
+    </div>;
   }
 }
 export default Event;
