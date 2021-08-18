798a62ee5861d2fe4a85746b49a851aa0a24df34
Akornefa Akyea <64564297+akornefa@users.noreply.github.com>
2021-08-17 20:49:56 -0500

    Added additional tests to render specific info in mock data

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -11,18 +11,35 @@ describe('<Event /> component', () => {
   });
 
   test('render details button', () => {
-    expect(EventWrapper.find('.detailsButton')).toHaveLength(2);
+    expect(EventWrapper.find('.detailsButton').text()).toEqual('Show More');
   });
 
   test('render event collapsed info', () => {
     expect(EventWrapper.find('.basicInfo')).toHaveLength(1);
+    expect(EventWrapper.find('.dateTime').text()).toEqual('2020-05-19T16:00:00+02:00');
+    expect(EventWrapper.find('.summary').text()).toEqual('Learn JavaScript');
+    expect(EventWrapper.find('.location').text()).toEqual('London, UK');
+
   });
 
   test('change state of details button onClick', () => {
     EventWrapper.setState({
       showDetails: false,
     });
-    EventWrapper.find('showDetails').simulate('click');
-    expect(EventWrapper.state('.showDetails')).toEqual(true);
+    EventWrapper.find('.detailsButton').simulate('click');
+    expect(EventWrapper.state('showDetails')).toEqual(true);
+
+    expect(EventWrapper.find('.eventDescription').text()).toEqual('Have you wondered how you can ask Google to show you the list of the top ten must-see places in London? And how Google presents you the list? How can you submit the details of an application? Well, JavaScript is doing these. :) \n\nJavascript offers interactivity to a dull, static website. Come, learn JavaScript with us and make those beautiful websites.');
+
+    EventWrapper.find('.detailsButton').simulate('click');
+    expect(EventWrapper.state('showDetails')).toEqual(false);
   });
+
+  test('render show less text on Button when expanded', () => {
+    EventWrapper.setState({
+      showDetails: true,
+    });
+    expect(EventWrapper.find('.detailsButton').text()).toEqual('Show Less');
+  });
+
 });
