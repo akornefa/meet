8440531c3b0add0977a62ff7b302a9c4b90b47e9
Akornefa Akyea <64564297+akornefa@users.noreply.github.com>
2021-08-21 15:35:41 -0500

    Use bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { getEvents, extractLocations } from './api';
 import './nprogress.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 class App extends Component {
@@ -12,14 +15,18 @@ class App extends Component {
   state = {
     events: [],
     locations: [],
-    eventsDisplayed: 32
+    eventsDisplayed: 32,
+    currentCity: 'all'
   }
 
   componentDidMount() {
     this.mounted = true;
     getEvents().then((events) => {
       if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
+        this.setState({
+          events: events.slice(0, this.state.eventsDisplayed),
+          locations: extractLocations(events)
+        });
       }
     });
   }
@@ -28,24 +35,41 @@ class App extends Component {
     this.mounted = false;
   }
 
-  updateEvents = (location) => {
+  //location is the citySuggestion that gets passed
+  // eventCount is the value that gets passed??
+  updateEvents = (location, eventCount) => {
     getEvents().then((events) => {
       const locationEvents = (location === 'all') ?
         events :
         events.filter((event) => event.location === location);
       this.setState({
-        events: locationEvents
+        events: locationEvents.slice(0, eventCount),
+        eventsDisplayed: eventCount
       });
     });
   }
 
   render() {
     return (
-      <div className='App'>
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-        <NumberOfEvents eventsDisplayed={this.state.eventsDisplayed} />
-        <EventList events={this.state.events} />
-      </div>
+      <Container>
+        <div className='App'>
+          <Row>
+            <Col md={6}>
+              <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <NumberOfEvents eventsDisplayed={this.state.eventsDisplayed} updateEvents={this.updateEvents} currentCity={this.state.currentCity} />
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <EventList events={this.state.events} />
+            </Col>
+          </Row>
+        </div>
+      </Container>
     );
   }
 }
