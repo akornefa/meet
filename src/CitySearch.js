982c08e982bc38c3947a2536b73385a8b0397aa5
Akornefa Akyea <64564297+akornefa@users.noreply.github.com>
2021-09-09 18:35:45 -0500

    import InfoAlert to CitySearch.js

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
 
   state = {
     query: '',
     suggestions: [],
-    showSuggestions: undefined
+    showSuggestions: undefined,
+    infoText: ''
   }
   // this simply takes the value from the input and updates the state of query based on that value.
   handleInputChanged = (event) => {
     const value = event.target.value;
+    this.setState({ showSuggestions: true });
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions
-    });
-  }
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText: 'We can not find the city you are looking for. Please try another city',
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions,
+        infoText: ''
+      });
+    }
+  };
 
   handleItemClicked = (suggestion) => {
     this.setState({
@@ -31,6 +42,8 @@ class CitySearch extends Component {
   render() {
     return (
       <div className='CitySearch'>
+        <InfoAlert text={this.state.infoText} />
+        <p>Search by city: </p>
         <input
           type='text'
           className='city'
